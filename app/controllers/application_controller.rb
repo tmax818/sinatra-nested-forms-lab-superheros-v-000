fe8672ad9df8156297ffcf583252a6db12edee8f
Tyler Maxwell <tmax818@mac.com>
2018-03-06 19:10:32 -0800

    initial commit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+get '/' do
+  erb :super_hero
+end
+
+post '/teams' do
+  @team = params[:team]
+  @team_name = params[:team][:name]
+  @team_motto = params[:team][:motto]
+  @team_members = params[:team][:members]
+  #binding.pry
+  erb :team
+end
 
 end
